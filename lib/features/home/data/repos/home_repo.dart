0a36f6651b8e2592856398549205a75fbb3a46ed
mohamedezz80 <madosharaf80@gmail.com>
2024-03-05 17:24:18 +0200

    impl fetch newest books

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -5,6 +5,6 @@ import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo
 {
-  Future<Either<Failure,List<BookModel>>> fetchBestSellerBooks();
+  Future<Either<Failure,List<BookModel>>> fetchNewestBooks();
   Future<Either<Failure,List<BookModel>>> fetchFeaturedBooks();
 }
